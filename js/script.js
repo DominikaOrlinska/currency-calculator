265521bf275d77584ae615a710615282cabf4e45
Dominika Orlinska <dominika.orlinska1@gmail.com>
2024-02-01 11:22:41 +0100

    Innitial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let formElement = document.querySelector(".js-form");
+
+let amountElement = document.querySelector(".js-amount");
+
+let currencyElement = document.querySelector(".js-currency");
+
+let resultElement = document.querySelector(".js-result");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let amount = amountElement.value;
+  let currency = currencyElement.value;
+  let result = resultElement.value;
+
+  let USD = 3.97;
+  let EURO = 4.35;
+
+  switch (currency) {
+    case "USD":
+      result = amount / USD;
+      break;
+    case "EURO":
+      result = amount / EURO;
+      break;
+  }
+
+  resultElement.innerText = result.toFixed(2);
+});
